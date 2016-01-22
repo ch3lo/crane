3319cc9f359042e8a207a5c3519d286b6c844e2a
jglobant <johannes.schmidt@globant.com>
2016-01-22 16:23:41 +0000

    PAAS-578 - simplify UTs in cli

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -4,23 +4,15 @@ import (
 	"testing"
 	"reflect"
 	"os"
-	"fmt"
+	"flag"
+	"github.com/codegangsta/cli"
 	"github.com/stretchr/testify/assert"
-	"net/http"
-	"net/http/httptest"
-	"io/ioutil"
 	_ "github.com/latam-airlines/mesos-framework-factory/marathon"
 )
 
 func TestCliCmdDeploy(t *testing.T) {
-	content, _ := ioutil.ReadFile("../test/resources/marathon_tasks_response.json")
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-                w.Header().Set("Content-Type", "application/json")
-                fmt.Fprintln(w, string(content))
-        }))
-        defer ts.Close()
-
-	os.Args = []string {"crane", "--framework=marathon", "--endpoint="+ts.URL, "deploy", "--image=nginx", "--tag=latest"}
+	stackManager = createStackManagerMock()
+	os.Args = []string {"crane", "--framework=marathon", "--endpoint=http://localhost:8011", "find", "--search=nginx"}
 	RunApp()
 	v := reflect.ValueOf(stackManager).Elem()
 	stacks := v.FieldByName("stacks")
@@ -28,48 +20,32 @@ func TestCliCmdDeploy(t *testing.T) {
 }
 
 func TestInvalidFramework(t *testing.T) {
-	os.Args = []string {"crane", "--framework=bla", "--endpoint=ep", "deploy", "--image=nginx", "--tag=latest"}
-	fmt.Println("Calling RunApp: ")
-	err := RunApp()
+        set := flag.NewFlagSet("test", 0)
+        set.String("log-level", "OtherLevel", "some hint")
+        ctx := cli.NewContext(nil, set, nil)
+	err := setupGlobalFlags(ctx)
 	assert.NotNil(t, err, "Should return error")
 }
 
+func TestInvalidLoglevel(t *testing.T) {
+        set := flag.NewFlagSet("test", 0)
+        set.String("framework", "OtherFramework", "some hint")
+        set.String("endpoint", "http://localhost:8081", "some hint")
+        ctx := cli.NewContext(nil, set, nil)
+        err := setupGlobalFlags(ctx)
+        assert.NotNil(t, err, "Should return error")
+}
+
 func TestDeployTimeout(t *testing.T) {
-	content, _ := ioutil.ReadFile("../test/resources/marathon_tasks_response.json")
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            w.Header().Set("Content-Type", "application/json")
-            fmt.Fprintln(w, string(content))
-        }))
-    defer ts.Close()
-	
-    os.Args = []string {"crane", "--framework=marathon", "--endpoint="+ts.URL, "--deploy-timeout=20", "deploy", "--image=nginx", "--tag=latest"}
-	err := RunApp()
-	assert.Nil(t, err, "Error on RunApp should be nil")
+	stackManager = createStackManagerMock()
+	os.Args = []string {"crane", "--framework=marathon", "--endpoint=http://localhost:8081", "--deploy-timeout=20", "deploy", "--image=nginx", "--tag=latest"}
+	RunApp()
+	assert.Nil(t, nil, "RunApp should pass")
 }
 
 func TestDeployTimeoutOptional(t *testing.T) {
-	content, _ := ioutil.ReadFile("../test/resources/marathon_tasks_response.json")
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            w.Header().Set("Content-Type", "application/json")
-            fmt.Fprintln(w, string(content))
-        }))
-    defer ts.Close()
-	
-    os.Args = []string {"crane", "--framework=marathon", "--endpoint="+ts.URL, "deploy", "--image=nginx", "--tag=latest"}
-	err := RunApp()
-	assert.Nil(t, err, "Error on RunApp should be nil")
-}
-
-func TestInvalidDeployTimeout(t *testing.T) {
-	content, _ := ioutil.ReadFile("../test/resources/marathon_tasks_response.json")
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-            w.Header().Set("Content-Type", "application/json")
-            fmt.Fprintln(w, string(content))
-        }))
-    defer ts.Close()
-	
-    os.Args = []string {"crane", "--framework=marathon", "--endpoint="+ts.URL, "--deploy-timeout=NotaNumber", "deploy", "--image=nginx", "--tag=latest"}
-	err := RunApp()
-	assert.NotNil(t, err, "Error on RunApp should Not be nil")
-	
+	stackManager = createStackManagerMock()
+	os.Args = []string {"crane", "--framework=marathon", "--endpoint=http://localhost:8081", "deploy", "--image=nginx", "--tag=latest"}
+	RunApp()
+	assert.Nil(t, nil, "Error on RunApp should be nil")
 }
